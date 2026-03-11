58d5c3bf78768c0bc8c710f1c4190b7fed3450de
I1HN <secundariaeduarda9@gmail.>
2026-03-11 10:21:13 -0300

    Criados calculo de dias de oxigenio restantes em marte

---
+++ script.js
@@ -0,0 +1,10 @@
+let oxigenio = Number(prompt("insira a quantidade de oxigenio restante (em litros)"));
+let pessoas = Number(prompt("insira a quantidade de pessoas em marte:"));
+const consumoDiario = 15; // litros por pessoa.
+
+//consumo total eh baseado na quantidade de pessoas multiplicado pelo consumo diário por pessoa.
+let consumoTotalBase = pessoas * consumoDiário;
+let diasRestantes = oxigenio / consumoTotalBase;
+
+alert(`relatorio de marte: restam ${diasRestantes.toFixed (0)} dias de oxigenio!!`);
+
