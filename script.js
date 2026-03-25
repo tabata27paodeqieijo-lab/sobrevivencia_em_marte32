14c94e454246c02c7dc844082b21ab3e664948d1
I1HN <lucasm.fatinatti@gmail.com>
2026-03-25 09:37:59 -0300

    Fiz a operação da soma

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let numDois;
 let result;
 
 numUm = Number(prompt("Escreva o primeiro número:"));
-numDois = Number(prompt('Escreva o segundo número:'));
+numDois = Number(prompt("Escreva o segundo número:"));
 result = numUm + numDois;
 
-alert (`O resultado da soma sera ${result.toFixed(2)}`);
+alert(`O resultado da soma sera ${result.toFixed(2)}`);
